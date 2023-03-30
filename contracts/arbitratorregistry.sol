129a4e5f57952f8ace573bc64f1d5aa234cf664e
Denis Smirnov <densmirnov@me.com>
2023-03-30 19:35:01 +0700

    Update arbitratorregistry.sol

--- contracts/arbitratorregistry.sol
+++ contracts/arbitratorregistry.sol
@@ -27,11 +27,13 @@ import "@kleros/erc-792/contracts/IArbitrator.sol";
 
 contract ArbitratorRegistry is Ownable
 {
-    event Deployed();
-    event ArbitratorAdded(uint256 indexed id, string name, IArbitrator arbitrator, bytes extraData);
-    event ExtraDataChanged(uint256 indexed id, bytes extraData);
-    event ArbitratorDeleted(uint256 indexed id);
+    event Deployed(); // Event emitted when the contract is deployed
+    event ArbitratorAdded(uint256 indexed id, string name, IArbitrator arbitrator, bytes extraData); // Event emitted when a new arbitrator is added to the registry
+    event ExtraDataChanged(uint256 indexed id, bytes extraData); // Event emitted when the extra data of an arbitrator is changed
+    event ArbitratorDeleted(uint256 indexed id); // Event emitted when an arbitrator is deleted from the registry
 
+
+    // Struct representing an arbitrator in the registry
     struct Arbitrator
     {
         string      name;
@@ -39,14 +41,15 @@ contract ArbitratorRegistry is Ownable
         bytes       extraData;
     }
 
-    uint256                        public counter;
-    mapping(uint256 => Arbitrator) public arbitrators;
+    uint256                        public counter;        // Counter to assign a unique ID to each arbitrator
+    mapping(uint256 => Arbitrator) public arbitrators;    // Mapping to store arbitrators by their ID
 
     constructor()
     {
         emit Deployed();
     }
 
+    // Function to add a new arbitrator to the registry
     function addArbitrator(string memory name, IArbitrator arb, bytes calldata extraData) public onlyOwner returns(uint256)
     {
         ++counter;
@@ -57,12 +60,14 @@ contract ArbitratorRegistry is Ownable
         return counter;
     }
 
+    // Function to update the extra data of an arbitrator in the registry
     function setExtraData(uint256 id, bytes calldata extraData) public onlyOwner
     {
         arbitrators[id].extraData = extraData;
         emit ExtraDataChanged(id, extraData);
     }
 
+    // Function to delete an arbitrator from the registry
     function deleteArbitrator(uint256 id) public onlyOwner
     {
         require(address(arbitrators[id].arbitrator) != address(0), "ArbitratorRegistry: not found");
@@ -70,21 +75,25 @@ contract ArbitratorRegistry is Ownable
         emit ArbitratorDeleted(id);
     }
 
+    // Function to check if an arbitrator exists in the registry
     function checkArbitrator(uint256 id) public view returns(bool)
     {
         return address(arbitrators[id].arbitrator) != address(0);
     }
 
+    // Function to get an arbitrator's details by their ID
     function arbitrator(uint256 id) public view returns(IArbitrator, bytes memory)
     {
         return (arbitrators[id].arbitrator, arbitrators[id].extraData);
     }
 
+    // Function to get the arbitration cost for an arbitrator by their ID
     function arbitrationCost(uint256 id) public view returns (uint256)
     {
         return arbitrators[id].arbitrator.arbitrationCost(arbitrators[id].extraData);
     }
 
+    // Function to get the appeal cost for an arbitrator by their ID and a dispute ID
     function appealCost(uint256 id, uint256 disputeId) public view returns (uint256)
     {
         return arbitrators[id].arbitrator.appealCost(disputeId, arbitrators[id].extraData);
